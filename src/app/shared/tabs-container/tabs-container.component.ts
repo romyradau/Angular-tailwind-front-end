c95906d6bc2070149f5a83f3fc24d44b81686684
Romy Schleicher <rschleic@1-E-11.42heilbronn.de>
2023-04-08 19:41:10 +0200

    tabs added

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit{
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
+
+  //chain operator
+  ngAfterContentInit(): void{
+    const activeTabs = this.tabs?.filter(
+      tab => tab.active
+    )
+
+    //bang operator
+    if (!activeTabs || activeTabs.length === 0){
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  //sets every active tab to false
+  selectTab(tab: TabComponent){
+    this.tabs?.forEach(tab =>{
+      tab.active = false
+    })
+
+    tab.active = true
+    
+    return false
+  }
 
 }
